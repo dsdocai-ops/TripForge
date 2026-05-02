efeeb73f6965e00f7b242c7a6916c1c16b88c27e
dsdocai-ops <dsdocai@gmail.com>
2026-05-01 23:57:09 -0700

    update

---
+++ TripForge/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TripForge from './TripForge'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TripForge />
+  </React.StrictMode>
+)
